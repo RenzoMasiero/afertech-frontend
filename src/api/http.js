a136c011ad010003659f32d6d577b71b88a7897c
RenzoMasiero88 <renzo.masiero.dev@gmail.com>
2026-03-09 21:51:48 -0300

    hardoce de link

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,11 @@
 import axios from "axios";
-
+/*
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+*/
+const api = axios.create({
+  baseURL: "https://afertech-backend-production.up.railway.app",
 });
 
 /* ===== REQUEST INTERCEPTOR ===== */
